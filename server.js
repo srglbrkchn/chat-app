f5c3d5edcc97f0a8cd43f077f0c6b6322574fb25
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-02-13 16:09:16 +0100

    fix security issue for sending message from server to browser.

---
+++ server.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+// const app = express();
+const cors = require("cors");
+
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
+
+// on user connection creates a new socket for the user
+io.on("connection", (socket) => {
+  // send a message through the socket to the user
+  socket.emit("chat-message", "Hi buddy!!!!!");
+});
