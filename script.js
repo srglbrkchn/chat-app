4423452c1b416f846719cdfc6e5a61c16242fd42
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2023-02-20 20:15:02 +0100

    build the backbone of the real time chat application. move to design.

--- script.js
+++ script.js
@@ -2,14 +2,38 @@ const socket = io("http://localhost:3000");
 
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
+const messageContainer = document.getElementById("message-container");
+const name = prompt("What's your name?");
+
+function appendMessage(message) {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageContainer.append(messageElement);
+}
+
+appendMessage("You joined!");
+socket.emit("new-user", name);
+
+socket.on("chat-message", (data) => {
+  if (data.message != "") {
+    appendMessage(`${data.name}: ${data.message}`);
+  }
+});
 
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInput.value;
   socket.emit("send-chat-message", message);
-  messageInput.value = "";
+  if (message != "") {
+    appendMessage(`You: ${message}`);
+    messageInput.value = "";
+  }
 });
 
-socket.on("chat-message", (data) => {
-  console.log(data);
+socket.on("user-connected", (name) => {
+  appendMessage(`${name} connected... `);
+});
+
+socket.on("user-disconnected", (name) => {
+  appendMessage(`${name} disconnected... `);
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // const express = require("express");
 // const app = express();
 const cors = require("cors");
+const users = {};
 
 const io = require("socket.io")(3000, {
   cors: {
@@ -13,9 +14,23 @@ const io = require("socket.io")(3000, {
 
 // on user connection creates a new socket for the user
 io.on("connection", (socket) => {
-  // send a message through the socket to the user
-  socket.emit("chat-message", "Hi buddy!!!!!");
-  socket.on("send-chat-message", (message) => {
-    socket.broadcast.emit("chat-message", message);
+  socket.on("new-user", (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit("user-connected", name);
+
+    // send a message through the socket to the user
+    // socket.emit("chat-message", "Hi buddy!!!!!");
+    socket.on("send-chat-message", (message) => {
+      socket.broadcast.emit("chat-message", {
+        message: message,
+        name: users[socket.id],
+      });
+    });
+
+    // when the user disconnects
+    socket.on("disconnect", () => {
+      socket.broadcast.emit("user-disconnected", users[socket.id]);
+      delete users[socket.id];
+    });
   });
 });
